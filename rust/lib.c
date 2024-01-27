8ad7ed0f93ddb5ced39e652bdd5d4804ae6e76de
Pedro Gabriel <pedro.planel@2gofintech.com.br>
2024-01-27 18:53:29 -0300

    fix: disable SIMSIMD_NATIVE_F16 and remove hard-coded compiler from build step

--- rust/lib.c
+++ rust/lib.c
@@ -1,5 +1,7 @@
 #include "simsimd/simsimd.h"
 
+#define SIMSIMD_NATIVE_F16 = 0
+
 simsimd_f32_t cosine_i8(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) {
     return simsimd_metric_punned(simsimd_metric_cosine_k, simsimd_datatype_i8_k, simsimd_cap_any_k)(a, b, d, d);
 }
